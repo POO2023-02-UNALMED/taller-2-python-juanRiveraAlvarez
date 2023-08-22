f775683899ecfb301f48cb8db75e8dc0259c9568
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2023-08-21 21:03:43 -0700

    intento1

---
+++ Asiento.py
@@ -0,0 +1,12 @@
+class Asiento:
+    def __init__(self,color,precio,registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(color):
+        colores = ["rojo","verde","amarillo","negro","blanco"]
+        if color in coles:
+            self.color  = color
+
+
---
+++ Motor.py
@@ -0,0 +1,12 @@
+class Motor:
+    def __init__(self, numeroCilindros,tipo,registro):
+        self.numeroCilindos=numeroCilindos
+        self.tipo = tipo
+        self.registro = registro
+
+    def carmbiarRegistro(registro):
+        self.registro =  registro
+
+    del asignarTipo(tipo):
+        if  tipo == "electrico" or tipo == "gasolina":
+            this.tipo = tipo
